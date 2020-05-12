f75aabf2af6b68a11526bb01e6dec2fb4e68ca78
apc <alexcurran.va@gmail.com>
2020-05-12 16:25:09 -0500

    Added date field to User model

--- models/User.js
+++ models/User.js
@@ -20,6 +20,10 @@ const UserSchema = new mongoose.Schema({
         required: true,
         min: 6,
         max: 1000
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
 });
 
