68d6b1aa918d89de02e2bbebc65e6b17eb1232e5
apc <alexcurran.va@gmail.com>
2020-05-12 17:08:59 -0500

    Added user registration with hashed password using bcrypt

--- routes/api/userRouter.js
+++ routes/api/userRouter.js
@@ -2,12 +2,51 @@
 
 const express = require("express");
 const router = express.Router();
+// User model
+const User = require("../../models/User");
+const bcrypt = require("bcrypt");
 
-router.post("/register", (req, res) => {
-    res.send("Registering user...");
+router.post("/register", async (req, res, next) => {
+    // TODO Add validation
+
+    // Check if user is in the db
+    const emailExists = await User.findOne({ email: req.body.email });
+
+    if (emailExists) {
+        return res.status(400).json({
+            message: "Email already exists."
+        });
+    }
+
+    // Hash password
+    const saltRounds = 12;
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+    // Create a new user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    });
+
+    try {
+        const savedUser = await user.save();
+
+        res.status(201).json({
+            message: "New user registered",
+            savedUser
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({
+            error: err
+        });
+
+        next(err);
+    }
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
     res.send("Logging user in...");
 });
 
