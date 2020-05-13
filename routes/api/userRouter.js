e8efacb3a002aecb0648a43fe234f76ad34e02e0
apc <alexcurran.va@gmail.com>
2020-05-13 11:00:17 -0500

    Login validation added

--- routes/api/userRouter.js
+++ routes/api/userRouter.js
@@ -18,28 +18,33 @@ router.post("/register", async (req, res, next) => {
     }
 
     // Check if user is in the db
-    const emailExists = await User.findOne({ email: req.body.email });
-
-    if (emailExists) {
-        return res.status(400).json({
-            message: "Email already exists."
-        });
+    try {
+        const emailExists = await User.findOne({ email: req.body.email });
+    
+        if (emailExists) {
+            return res.status(400).json({
+                message: "Email already exists."
+            });
+        }
+    } catch (err) {
+        console.error(err);
     }
 
-    // Hash password
-    const saltRounds = 12;
-    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-
-    // Create a new user
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-    });
-
+    
     try {
-        const savedUser = await user.save();
+        // Hash password
+        const saltRounds = 12;
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    
+        // Create a new user
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
 
+        const savedUser = await user.save();
+        
         res.status(201).json({
             message: "New user registered",
             user: user._id
@@ -54,8 +59,47 @@ router.post("/register", async (req, res, next) => {
     }
 });
 
-router.post("/login", async (req, res) => {
-    res.send("Logging user in...");
+router.post("/login", async (req, res, next) => {
+    // Validation
+    try {
+        await loginValidation(req.body);
+    } catch (error) {
+        return res.status(400).json({
+            error: error.details[0].message
+        });
+    }
+
+    // Check if email exists
+    try {
+        const user = await User.findOne({ email: req.body.email });
+    
+        if (!user) {
+            return res.status(400).json({
+                message: "Email is not found"
+            });
+        }
+
+        // Validate password
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                error: "Invalid password"
+            });
+        }
+
+        res.status(200).json({
+            message: "Logged in!"
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({
+            error: err
+        });
+
+        next(err);
+    }
+
 });
 
 module.exports = router;
