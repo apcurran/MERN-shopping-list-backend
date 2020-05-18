07c72d489e503d195cac89c9f3d5827e486aa0dd
apc <alexcurran.va@gmail.com>
2020-05-18 16:35:37 -0500

    Final built public folder containing react production build

---
+++ public/precache-manifest.31e680503bc4cba52f7ef1bf9f0dc126.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e923b8794b3877a6db03aee8307fe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23738c8745c05b4c0f4e",
+    "url": "/static/css/main.2179c487.chunk.css"
+  },
+  {
+    "revision": "3081ad7f6817b6748a8d",
+    "url": "/static/js/2.d594a9f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d594a9f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23738c8745c05b4c0f4e",
+    "url": "/static/js/main.48eb7fec.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "2cab0046d32d58c983af1acb002837cc",
+    "url": "/static/media/icon-logout.2cab0046.svg"
+  },
+  {
+    "revision": "297e854fe58256020de9cbde7deaebb1",
+    "url": "/static/media/icon-trash.297e854f.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use("/api/user", userRoute);
 // Handle production assets
 if (process.env.NODE_ENV === "production") {
     // Set static folder
-    app.use(express.static("client/build"));
+    app.use(express.static(__dirname + "/public/"));
     // Handle SPA
     app.get(/.*/, (req, res) => {
-        res.sendFile(__dirname + "client/build/index.html");
+        res.sendFile(__dirname + "/public/index.html");
     });
 }
 
