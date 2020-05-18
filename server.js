73d6911913dd5f076d99088ba96ea53a23eb3a59
apc <alexcurran.va@gmail.com>
2020-05-18 16:02:37 -0500

    Ready for production

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const compression = require("compression");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -16,9 +17,20 @@ mongoose
 
 // Setup body-parser
 app.use(express.json());
+app.use(compression()); // Compress all routes
 
 // Routes Middleware
 app.use("/api/user/items", itemRoute);
 app.use("/api/user", userRoute);
+
+// Handle production assets
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+    // Handle SPA
+    app.get(/.*/, (req, res) => {
+        res.sendFile(__dirname + "client/build/index.html");
+    });
+}
 
 app.listen(PORT, () => console.log(`Listening on port, ${PORT}.`));
